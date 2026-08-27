c09f3f8779c0882756b8ed12979462521f7e128d
Lche <m506108@outlook.com>
2026-08-27 17:56:44 +0800

    ChatPromptTemplate

--- practice/PromptTemplate.py
+++ practice/PromptTemplate.py
@@ -1,4 +1,4 @@
-from langchain_core.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate, ChatPromptTemplate
 from dotenv import load_dotenv
 import os
 load_dotenv(encoding="utf-8")
@@ -21,3 +21,37 @@ template3 = PromptTemplate.from_template("请用一句话介绍{content}"
 )
 prompt3 = template3.format(content="agent",length=50)
 print(prompt3)
+
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
+
+messages = [
+    SystemMessage(content="你是一个Agent工程师"),
+    HumanMessage(content="告诉我agent流程"),
+    AIMessage(content="好的，我将告诉你详细流程")
+]
+###########
+chat_prompt = ChatPromptTemplate.from_messages([
+        ("system","你是一个{role}，请回答我提出的问题"),
+        ("human", "请回答：{question}")
+    ]
+)
+
+message = chat_prompt.format_messages(
+    role="agent工程师",
+    questions="agent怎么建造"
+)
+result = model.invoke(messages)
+
+# 方式二：得到 ChatPromptValue
+prompt_value = chat_prompt.invoke({
+    "role": "python开发工程师",
+    "question": "快速排序怎么写"
+})                        
+result = model.invoke(prompt_value)
+
+# 方式三：得到纯字符串
+prompt_str = chat_prompt.format(
+    role="python开发工程师",
+    question="快速排序怎么写"
+)
+print(prompt_str)
