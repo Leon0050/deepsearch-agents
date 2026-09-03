a188fcee4884b27b4a707a4e579d6e24bb8a5eed
Lche <m506108@outlook.com>
2026-09-03 20:24:50 +0800

    weather api tool

--- practice/tools.py
+++ practice/tools.py
@@ -1,6 +1,12 @@
 from langchain.tools import tool
 from loguru import logger
 from pydantic import BaseModel, Field
+import httpx
+import os
+import json
+from dotenv import load_dotenv
+load_dotenv(encoding="utf-8")
+
 
 # 1
 # @tool
@@ -31,3 +37,23 @@ logger.info(f"return_direct = {add_number.return_direct}")
 result = add_number.invoke({"a":1, "b":2})
 print(result)
 logger.info(result)
+
+@tool
+def get_weather(loc:str) -> str:
+    """
+        查询指定城市的即时天气。
+    """
+    url = "https://api.openweathermap.org/data/2.5/weather"
+    params={
+        "q":loc,
+        "appid":os.getenv("OPENWEATHER_API_KEY"),
+        "units":"metric",
+        "lang":"zh_cn"
+    }
+    response = httpx.get(url, params=params, timeout=30)
+    data = response.json()
+    return json.dumps(data)
+
+result = get_weather.invoke("Chuzhou")
+print(result)
+
